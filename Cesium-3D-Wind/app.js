0b62ba98ad282cfe20f5cec95080095a2e33db52
P.Zhao <840882905@qq.com>
2022-07-15 14:15:33 +0800

    着色器代码调试

--- Cesium-3D-Wind/app.js
+++ Cesium-3D-Wind/app.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: P.Zhao
+ * @Company: HT
+ * @Date: 2022-07-06 15:56:57
+ * @LastEditors: P.Zhao
+ * @LastEditTime: 2022-07-12 14:18:26
+ * @Description:
+ * @FilePath: \3D-Wind-Field\Cesium-3D-Wind\app.js
+ */
 //prettier-ignore
 demo = true
 const mode = {
   debug: demo ? false : true
 }
-
+debugger
 var panel = new Panel()
 //prettier-ignore
 var wind3D = new Wind3D(
--- Cesium-3D-Wind/glsl/segmentDraw.frag
+++ Cesium-3D-Wind/glsl/segmentDraw.frag
@@ -1,4 +1,5 @@
 void main() {
     const vec4 white = vec4(1.0);
+    //gl_FragColor = vec4(1.0,0.0,0.0,1.0);
     gl_FragColor = white;
 }
